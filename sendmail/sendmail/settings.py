6e2d7e756323510ee2074e687a5401bb15710979
sahil <srhussain.sr@gmail.com>
2022-03-22 13:20:51 +0530

    changes to static

--- sendmail/sendmail/settings.py
+++ sendmail/sendmail/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
 MEDIA_URL = '/media/'
